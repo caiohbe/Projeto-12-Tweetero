1b34b884aa522b4cacd6d532477bdc68eb3783a4
caiohbe <caiohbe@gmail.com>
2022-11-05 14:46:38 -0300

    fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.post('/sign-up', (req, res) => {
     const { username, avatar} = req.body
 
     if (!username || !avatar) {
-        console.log('Insira todos os campos')
+        res.sendStatus(400)
         return
     }
 
@@ -18,7 +18,8 @@ app.post('/sign-up', (req, res) => {
         avatar
     })
 
-    res.send(usuarios)
+    //res.send(usuarios)
+    res.sendStatus(200)
 }) 
 
 app.listen(5000, () => console.log("app running at 5000"))
