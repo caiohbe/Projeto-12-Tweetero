7eef9a2740f0f9cb26947032fb52da19677e9dfe
caiohbe <caiohbe@gmail.com>
2022-11-05 15:56:51 -0300

    sign-up post, tweets get

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from 'express'
+import cors from 'cors'
 
-const app = express()
+const app = express();
+
+app.use(cors())
 app.use(express.json())
 
 const usuarios = []
+
+const tweets = [
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+]
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar} = req.body
@@ -21,5 +42,14 @@ app.post('/sign-up', (req, res) => {
     //res.send(usuarios)
     res.sendStatus(200)
 }) 
+
+app.get('/tweets', (req, res) => {
+    res.send(tweets)
+})
+
+app.post('/tweets', (req, res) => {
+    console.log(req.body)
+    res.send('foi')
+})
 
 app.listen(5000, () => console.log("app running at 5000"))
