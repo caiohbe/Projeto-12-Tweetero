91c04c7851a5e833f60f1174b9d9e34d0bd6e779
caiohbe@gmail.com <caiohbe@gmail.com>
2022-11-06 08:58:39 -0300

    API 1.0

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const usuarios = []
+const usuarios = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    }
+]
 
 const tweets = [
 	{
@@ -24,6 +33,41 @@ const tweets = [
 		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
 		tweet: "eu amo o hub"
 	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	}
 ]
 
 app.post('/sign-up', (req, res) => {
@@ -39,17 +83,31 @@ app.post('/sign-up', (req, res) => {
         avatar
     })
 
-    //res.send(usuarios)
-    res.sendStatus(200)
+    res.send("OK")
 }) 
 
-app.get('/tweets', (req, res) => {
-    res.send(tweets)
+app.post('/tweets', (req, res) => {
+    const { username, tweet} = req.body
+    const avatar = usuarios[usuarios.length-1].avatar
+
+    if (!username || !tweet || !avatar) {
+        res.sendStatus(400)
+        return
+    }
+
+    const tweetData = {
+        username,
+        avatar,
+        tweet
+    }
+
+    tweets.push(tweetData)
+
+    res.send('OK')
 })
 
-app.post('/tweets', (req, res) => {
-    console.log(req.body)
-    res.send('foi')
+app.get('/tweets', (req, res) => {
+    res.send(tweets.slice(-10))
 })
 
 app.listen(5000, () => console.log("app running at 5000"))
